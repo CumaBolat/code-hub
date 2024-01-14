ca45e4f2becca722d22c0553fef6d21cfa87a199
Cuma Bolat <cuma.bolat@toptal.com>
2024-01-14 23:19:03 +0300

    added minimal home page

--- src/main/frontend/src/components/FileBrowser.js
+++ src/main/frontend/src/components/FileBrowser.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import '../css/filebrowser.css';
 
-function FileBrowser( { setWs, ws, code, setCode, getFilesList, files}) {
+function FileBrowser({ setWs, ws, code, setCode, getFilesList, files }) {
   const [isListVisible, setListVisible] = useState(true);
   const [isPopupVisible, setPopupVisible] = useState(false);
   const [newFileName, setNewFileName] = useState('');
@@ -10,7 +10,7 @@ function FileBrowser( { setWs, ws, code, setCode, getFilesList, files}) {
   const toggleListVisibility = () => {
     setListVisible(!isListVisible);
   };
-  
+
   const openFile = (file) => {
     fetch('http://localhost:5000/openFile', { method: 'POST', body: file })
       .then(response => response.text())
@@ -21,7 +21,7 @@ function FileBrowser( { setWs, ws, code, setCode, getFilesList, files}) {
   };
 
   const createFile = (fileName) => {
-    fetch('http://localhost:5000/createFile', { method: 'POST', body: JSON.stringify(fileName)})
+    fetch('http://localhost:5000/createFile', { method: 'POST', body: JSON.stringify(fileName) })
       .then(response => response.text())
       .then(data => setCode(data))
       .catch((error) => {
@@ -36,20 +36,23 @@ function FileBrowser( { setWs, ws, code, setCode, getFilesList, files}) {
     }
     setPopupVisible(true);
   };
-  
+
   const handleFileNameChange = (event) => {
     setNewFileName(event.target.value);
   };
-  
+
   const handleCreateFile = () => {
     createFile(newFileName);
     setTimeout(getFilesList, 100);
     setPopupVisible(false);
   };
-  
-  return(
+
+  return (
     <div className="browser">
-      <h1 id="browser-header" >FileBrowser</h1>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <button onClick={() => window.history.back()} style={{ marginRight: '10px' }}>Go Back</button>
+        <h1 id="browser-header">FileBrowser</h1>
+      </div>
 
       <div>
         <button className="list-button" onClick={toggleListVisibility}>
@@ -59,11 +62,11 @@ function FileBrowser( { setWs, ws, code, setCode, getFilesList, files}) {
         <button className="create-file-button" onClick={handleCreateFileClick} >Create File</button>
 
         {isPopupVisible && (
-        <div>
-          <input type="text" value={newFileName} onChange={handleFileNameChange} placeholder="Enter file name" />
-          <button className="file-button" onClick={handleCreateFile}>OK</button>
-        </div>
-      )}
+          <div>
+            <input type="text" value={newFileName} onChange={handleFileNameChange} placeholder="Enter file name" />
+            <button className="file-button" onClick={handleCreateFile}>OK</button>
+          </div>
+        )}
 
         {isListVisible && (
           <ul>
@@ -78,27 +81,27 @@ function FileBrowser( { setWs, ws, code, setCode, getFilesList, files}) {
       <div className="docs">
         <h2 id="editor-header">Getting Started with OnlineIDE</h2>
         <ol id="editor-text">
-            <li>Establish a WebSocket Connection</li>
-            <li>Create Your Personal Workspace</li>
-            <li>Start Coding</li>
-            <li>Submit Your Code</li>
+          <li>Establish a WebSocket Connection</li>
+          <li>Create Your Personal Workspace</li>
+          <li>Start Coding</li>
+          <li>Submit Your Code</li>
         </ol>
         <h2 id="editor-header">Key Features</h2>
         <ul id="editor-text">
-            <li>Advanced Syntax Highlighting</li>
-            <li>Semi Auto-Completion</li>
-            <li>Efficient File Browsing</li>
-            <li>Easy File Creation</li>
-            <li>Integrated Terminal</li>
-            <li>Secure Execution of Commands in Docker Container</li>
+          <li>Advanced Syntax Highlighting</li>
+          <li>Semi Auto-Completion</li>
+          <li>Efficient File Browsing</li>
+          <li>Easy File Creation</li>
+          <li>Integrated Terminal</li>
+          <li>Secure Execution of Commands in Docker Container</li>
         </ul>
         <h2 id="editor-header">Technologies Used</h2>
         <ul id="editor-text">
-            <li>Backend: Java, Spring Boot</li>
-            <li>Frontend: JavaScript, HTML, CSS, React</li>
-            <li>Containerization: Docker</li>
-            <li>Deployment: AWS</li>
-            <li>Additional Technologies: REST APIs, WebSockets</li>
+          <li>Backend: Java, Spring Boot</li>
+          <li>Frontend: JavaScript, HTML, CSS, React</li>
+          <li>Containerization: Docker</li>
+          <li>Deployment: AWS</li>
+          <li>Additional Technologies: REST APIs, WebSockets</li>
         </ul>
       </div>
     </div>
--- src/main/frontend/src/components/Home.js
+++ src/main/frontend/src/components/Home.js
@@ -1,28 +1,108 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'; // Import Font Awesome icons
 
 import '../css/home.css';
 
 function LoginRegisterButtons() {
   const navigate = useNavigate();
 
-  const handleLogin = async () => {
-    navigate('/login');
-  };
-
-  const handleRegister = async () => {
-    navigate('/register');
+  const hanleGameOfLife = async () => {
+    navigate('/game-of-life');
   };
 
   const handleEditor = () => {
     navigate('/code-editor');
   }
 
+  function centerSquare() {
+    const innerSquare = document.getElementById('inner-square');
+    const outerSquare = document.getElementById('outer-square');
+    const centerButton = document.getElementById('center-button');
+
+    const outerWidth = outerSquare.offsetWidth;
+    const outerHeight = outerSquare.offsetHeight;
+
+    const centerTop = (outerHeight - innerSquare.offsetHeight) / 2;
+    const centerLeft = (outerWidth - innerSquare.offsetWidth) / 2;
+
+    if (innerSquare.style.top === `${centerTop}px` && innerSquare.style.left === `${centerLeft}px`) {
+
+      const randomTop = Math.random() * (outerHeight - innerSquare.offsetHeight);
+      const randomLeft = Math.random() * (outerWidth - innerSquare.offsetWidth);
+
+      centerButton.innerHTML = 'Center div';
+      innerSquare.style.top = `${randomTop}px`;
+      innerSquare.style.left = `${randomLeft}px`;
+    } else {
+      centerButton.innerHTML = 'Shuffle';
+      innerSquare.style.top = `${centerTop}px`;
+      innerSquare.style.left = `${centerLeft}px`;
+    }
+  }
+
   return (
-    <div className="button-container">
-      <button className="pretty-button" onClick={handleLogin}>Login</button>
-      <button className="pretty-button" onClick={handleRegister}>Register</button>
-      <button className="pretty-button" onClick={handleEditor}>Editor</button>
+    <div className="center-container">
+      <div id='outer-square' className="outer-square">
+        <div id="inner-square" className="inner-square"></div>
+      </div>
+      <button id='center-button' className='home-page-center-button' onClick={centerSquare}>Center Div</button>
+
+      <div className="content">
+        <header>
+          <h1 className='home-header-1'>Hey, I'm Cuma!</h1>
+          <h2 className='home-header-2'>Software Engineer</h2>
+        </header>
+
+        <section className="about-me">
+        <p>
+          Hello👋 I'm Cuma Bolat, a software engineer with 2 years of experience specializing in
+          Backend Development. I have a knack for Data Structures and Algorithms, turning coding
+          challenges into triumphs. And yes, I also know how to center a div!
+        </p>
+        <p>
+          Outside of my algorithmic adventures, I enjoy working on challenging projects
+          that push my boundaries as a developer. Feel free to check out some of my work below.
+        </p>
+        <p>
+          I'm always open to new opportunities and learning experiences. If you have an 
+          interesting project or just want to discuss tech, don't hesitate to reach out!
+          You can find my contact info in the footer.
+        </p>
+        </section>
+
+        <section className='skills'>
+          <div className='skill'>
+            <h3 className='home-header-3'>Technologies</h3>
+            <ul>
+              <li className='skill-item'><span>Programming Languages:</span> Java, Ruby, PHP, JavaScript, HTML, CSS, C, SQL</li>
+              <li className='skill-item'><span>Frameworks & Libraries:</span> Spring Boot, Ruby on Rails, React, RSpec, Swing</li>
+              <li className='skill-item'><span>Databases:</span> PostgreSQL, MySQL, SQLite</li>
+              <li className='skill-item'><span>Development Tools:</span> Git, GitHub, Jira, Docker, AWS</li>
+              <li className='skill-item'><span>Methodologies:</span> Scrum, Agile Development, TDD, BDD, CD, CI, OOP</li>
+              <li className='skill-item'><span>Soft Skills:</span> Adaptability, Motivation, Problem Solving, Work Ethic, Teamwork, Leadership, Communication</li>
+            </ul>
+          </div>
+        </section>
+
+        <div className="button-container">
+          <button className='home-button disabled-button' onClick={hanleGameOfLife}>Game of Life (Coming Soon)</button>
+          <button className='home-button' onClick={handleEditor}>Online Code Editor</button>
+        </div>
+      </div>
+
+      <footer>
+        <a href="https://github.com/CumaBolat" target="_blank" rel="noopener noreferrer">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://www.linkedin.com/in/cuma-bolat-991682200/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin size={30} />
+        </a>
+
+        <a href="mailto:cumabolat@posta.mu.edu.tr" target="_blank" rel="noopener noreferrer">
+          <FaEnvelope size={30} />
+        </a>
+      </footer>
     </div>
   );
 }
