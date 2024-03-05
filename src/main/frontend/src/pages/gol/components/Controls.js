f73f4ac6071b591d7bcec7382e2ebf9ae6849aea
Cuma Bolat <cuma.bolat@toptal.com>
2024-03-05 07:10:22 +0300

    little frontend tweaks

--- src/main/frontend/src/pages/gol/components/Controls.js
+++ src/main/frontend/src/pages/gol/components/Controls.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Popup from 'reactjs-popup';
 
 import { useGlobalContext } from "../../../GlobalContext";
@@ -9,8 +9,10 @@ import "../css/popup.css";
 
 const Controls = ({ gridSize, setGridSize, grid, setGrid }) => {
   const { sessionId, ws, setWs, stompClient } = useGlobalContext();
-  const [patternName, setPatternName] = React.useState("");
-  const [patternDescription, setPatternDescription] = React.useState("");
+  const [patternName, setPatternName] = useState("");
+  const [patternDescription, setPatternDescription] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+  const [selectedPattern, setSelectedPattern] = useState(null);
 
   const startGame = () => {
     console.log("Start Game");
@@ -26,34 +28,28 @@ const Controls = ({ gridSize, setGridSize, grid, setGrid }) => {
     });
 
     ws.send('/app/gameoflife/start', { "simpSessionId": sessionId }, serializedGrid);
+    setGameStarted(true);
   };
 
   const stopGame = () => {
     ws.send('/app/gameoflife/stop', { "simpSessionId": sessionId }, '');
+    setGameStarted(false);
   }
 
   const clearGrid = () => {
-    const newGrid = [];
-    for (let i = 0; i < gridSize; i++) {
-      const row = [];
-      for (let j = 0; j < gridSize; j++) {
-        row.push(0);
-      }
-      newGrid.push(row);
-    }
+    const newGrid = Array.from({ length: gridSize }, () => Array.from({ length: gridSize }, () => 0));
     setGrid(newGrid);
+    setPatternName("");
+    setPatternDescription("");
+    setSelectedPattern(null);
   }
 
   const randomizeGrid = () => {
-    const newGrid = [];
-    for (let i = 0; i < gridSize; i++) {
-      const row = [];
-      for (let j = 0; j < gridSize; j++) {
-        row.push(Math.random() > 0.85 ? 1 : 0);
-      }
-      newGrid.push(row);
-    }
+    const newGrid = Array.from({ length: gridSize }, () => Array.from({ length: gridSize }, () => Math.random() > 0.75 ? 1 : 0));
     setGrid(newGrid);
+    setPatternName("");
+    setPatternDescription("");
+    setSelectedPattern(null);
   }
 
   const handleSpeedChange = (e) => {
@@ -67,6 +63,7 @@ const Controls = ({ gridSize, setGridSize, grid, setGrid }) => {
   const handlePreFill = (e) => {
     const pattern = e.target.value;
     setPatternName(pattern.replace(/_/g, ' '));
+    setSelectedPattern(pattern);
     fetch(`http://localhost:5000/pattern?pattern=${pattern}`, {
       method: 'GET',
       headers: {
@@ -113,12 +110,12 @@ const Controls = ({ gridSize, setGridSize, grid, setGrid }) => {
       </div>
       <div className="input-container">
         <label htmlFor="gridSize">Scale:</label>
-        <input id="gridSize" type="range" min="20" max="100" defaultValue={"20"} onChange={handleGridSize} />
+        <input id="gridSize" type="range" min="20" max="100" defaultValue={"20"} onChange={handleGridSize} disabled={gameStarted} />
       </div>
       <div className="options">
         <label htmlFor="preFill">Patterns:</label>
-        <select onChange={handlePreFill}>
-          <option hidden disabled selected value> Select a Pattern </option>
+        <select onChange={handlePreFill} disabled={gameStarted}>
+          <option hidden disabled selected={!selectedPattern} value> Select a Pattern </option>
           <option value="STABLE_REFLECTOR">STABLE_REFLECTOR</option>
           <option value="GUNSTAR">GUNSTAR</option>
           <option value="55P23">55P23</option>
@@ -130,10 +127,10 @@ const Controls = ({ gridSize, setGridSize, grid, setGrid }) => {
         <p className="pattern-description">{patternDescription}</p>
       </div>
       <div className="control">
-        <button className="control-button" onClick={startGame}>Start</button>
-        <button className="control-button" onClick={stopGame}>Stop</button>
-        <button className="control-button" onClick={clearGrid}>Clear</button>
-        <button className="control-button" onClick={randomizeGrid}>Randomize</button>
+        <button className="control-button" onClick={startGame} disabled={gameStarted}>Start</button>
+        <button className="control-button" onClick={stopGame} disabled={!gameStarted}>Stop</button>
+        <button className="control-button" onClick={clearGrid} disabled={gameStarted}>Clear</button>
+        <button className="control-button" onClick={randomizeGrid} disabled={gameStarted}>Randomize</button>
         <Popup
           trigger={<button className="control-button">FAQ</button>}
           modal
@@ -141,14 +138,13 @@ const Controls = ({ gridSize, setGridSize, grid, setGrid }) => {
         >
           {(close) => (
             <div className="modal">
-              <FAQ onClose={close} /> {}
+              <FAQ onClose={close} />
             </div>
           )}
         </Popup>
       </div>
       {ProjectDescription()}
     </div>
-
   );
 }
 
